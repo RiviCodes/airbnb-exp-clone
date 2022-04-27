e9bbe5acc231a5c60b5abe94e51edffd3f92a97b
RiviCodes <jose.riveramz@udlap.mx>
2022-04-27 10:58:30 -0500

    Add components folder

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/Navbar.css";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <img
+        src="../images/airbnb-logo.png"
+        alt="airbnb-logo"
+        className="navbar-logo"
+      />
+    </nav>
+  );
+}
