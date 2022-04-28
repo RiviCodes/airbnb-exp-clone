64e42be6db29d11087a1c6862d9836cb90303087
RiviCodes <jose.riveramz@udlap.mx>
2022-04-28 15:29:53 -0500

    Fix props passing

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,17 +2,26 @@ import React from "react";
 import "../styles/Card.css";
 
 export default function Card(props) {
+  /* Determines if the bagde should render & the text inside it */
+  let badgeText;
+  if (props.item.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.item.isOnline === true) {
+    badgeText = "ONLINE";
+  }
+
   return (
     <div className="card">
-      <img src={`../images/${props.img}`} className="card--image" />
+      {badgeText && <div className="card--badge">{badgeText}</div>}
+      <img src={`../images/${props.item.img}`} className="card--image" />
       <div className="card--stats">
         <img className="card--star" src="../images/star-on.png" />
-        <span>{props.rating}</span>
-        <span className="grey">{`(${props.reviewCount}) • ${props.country}`}</span>
+        <span>{props.item.rating}</span>
+        <span className="grey">{`(${props.item.reviewCount}) • ${props.item.country}`}</span>
       </div>
-      <p className="card--title">{props.title}</p>
+      <p className="card--title">{props.item.title}</p>
       <p className="card--price">
-        <span className="p-bold">From ${props.price}</span> / person
+        <span className="p-bold">From ${props.item.price}</span> / person
       </p>
     </div>
   );
