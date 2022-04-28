f50fd068dcdc09ee8dc7cd2e405249ec53782d59
RiviCodes <jose.riveramz@udlap.mx>
2022-04-28 10:49:27 -0500

    Add function to render x instances of the Card component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Card from "./components/Card";
+import peopleData from "./peopleData";
 
 function App() {
+  const cards = peopleData.map((item) => {
+    return (
+      <Card
+        img={item.img}
+        rating={item.rating}
+        reviewCount={item.reviewCount}
+        country={item.country}
+        title={item.title}
+        price={item.price}
+      />
+    );
+  });
+
   return (
     <>
       <Navbar />
       <Hero />
+      <section className="card-list">{cards}</section>
     </>
   );
 }
