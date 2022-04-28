1aa6b1a40f1850edf9ee924a914e5649f09aefc9
RiviCodes <jose.riveramz@udlap.mx>
2022-04-28 10:53:48 -0500

    Add array with default information

---
+++ src/peopleData.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    img: "nick.jpg",
+    rating: "4.2",
+    reviewCount: 4,
+    country: "Canada",
+    title: "Golden hour with Nick S.",
+    price: 49,
+  },
+  {
+    img: "sumire.jpg",
+    rating: "4.7",
+    reviewCount: 7,
+    country: "Japan",
+    title: "Sumire's Gardening Tales",
+    price: 35,
+  },
+  {
+    img: "matt.jpg",
+    rating: "4.9",
+    reviewCount: 9,
+    country: "Colombia",
+    title: "A Perfect Cup by Matt",
+    price: 42,
+  },
+];
