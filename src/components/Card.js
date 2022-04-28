182d21b72883e46c0350847a73eb4dff40bc8919
RiviCodes <jose.riveramz@udlap.mx>
2022-04-28 10:52:16 -0500

    Add Card component, add style .css for the component

---
+++ src/components/Card.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../styles/Card.css";
+
+export default function Card(props) {
+  return (
+    <div className="card">
+      <img src={`../images/${props.img}`} className="card--image" />
+      <div className="card--stats">
+        <img className="card--star" src="../images/star-on.png" />
+        <span>{props.rating}</span>
+        <span className="grey">({props.reviewCount}) • </span>
+        <span className="grey">{props.country}</span>
+      </div>
+      <p className="card--title">{props.title}</p>
+      <p className="card--price">
+        <span className="p-bold">From ${props.price}</span> / person
+      </p>
+    </div>
+  );
+}
